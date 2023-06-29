cde5cf26526c1f1e87c648046b02b14a3e873564
Eric Mugo <mugoericpg20@gmail.com>
2023-06-29 11:24:25 +0300

    index,show,create

--- app/controllers/tenant_controller.rb
+++ app/controllers/tenant_controller.rb
@@ -1,3 +1,27 @@
 class TenantController < ApplicationController
-    
+    before_action :set_tenant, only: [:show, :update, :destroy ]
+
+    def index
+        @tenant = Tenant.all
+        render json: @tenants
+    end
+
+    def show
+        render json: @tenant
+    end
+
+    def create
+        @tenant = Tenant.new(tenant_params)
+
+        if tenant.save 
+            render json: @tenant, status: :created
+        else
+            render json: @tenant.errors, status: :unprocessible_entity
+        end
+
+    end
+
+
+
+
 end
